9a1b7b45376e724d547b185fb308680112ca58c8
Brylan Gannon <brylan.gannon112@gmail.com>
2021-12-15 12:49:12 -0700

    Model a dinosaur

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+# hash model of a dinosaur
+dinosaur = {
+  species: "T-rex",
+  diet: "Carnivorous",
+  teeth_are_sharp: true,
+  has_horns: false,
+  sets_of_claws: 4, # hands and feet
+  claw_length: 4.5, # inches
+  potential_threats: ["Spinosaurus", "T-rex", "Pterodactyl"],
+  preffered_prey: ["Tyrannosaurus", "Edmontosaurus", "Triceratops"]
+}
