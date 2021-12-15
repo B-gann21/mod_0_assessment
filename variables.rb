19c20268e5e978fc7e2ccdbdac5f339cd444ddd0
Brylan Gannon <brylan.gannon112@gmail.com>
2021-12-15 12:40:59 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,13 @@
+movie = "R.I.P.D."
+is_entertaining = true
+movie = "The Joker"
+
+num_4 = 34
+num_6 = 67
+product = num_4 * num_6
+
+favorite_foods = ["Pizza", "Chile rellenos", "Shepherd's pie"]
+favorite_foods.delete_at(0)
+puts favorite_foods
+
+puts movie.length
