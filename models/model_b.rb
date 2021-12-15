fd6c2d1b7f5a2f9e2fe1ad9f5a743198e6024617
Brylan Gannon <brylan.gannon112@gmail.com>
2021-12-15 12:55:39 -0700

    Model a developer

---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+# hash model of a developer (myself)
+developer = {
+  name: "Brylan",
+  last_name: "Gannon",
+  knows_git: true,
+  in_tech_school: true,
+  years_experience: 0,
+  years_educated: 0.5,
+  outside_tech_resources: ["Garrick McComick", "SoloLearn app", "Codecademy"],
+  outside_personal_resources: ["Mia Underwood", "Jonathan Gannon", "Zoe Patten"]
+}
